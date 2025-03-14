8e23fe00f7820246917132a3123bfae25badd2a7
samee-k <sameeksha@fusemachines.com>
2025-03-14 16:05:40 +0545

    add: config files

---
+++ libs/config.js
@@ -0,0 +1,5 @@
+export const BASE_URL =
+    __ENV.BASE_URL || "https://app-api-dev.fuseclassroom.com";
+export const ORIGIN = __ENV.ORIGIN || "https://thames-dev.fuseclassroom.com";
+export const TOKEN = __ENV.TOKEN || "default_token_here";
+export const ID_TOKEN = __ENV.ID_TOKEN || "default_id_token_here";
