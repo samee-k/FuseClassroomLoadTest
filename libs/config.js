766992dd97b52c34b513cd1979065d33ac0debb8
samee-k <sameeksha@fusemachines.com>
2026-03-19 13:28:00 +0545

    update: prod config added but commented

--- libs/config.js
+++ libs/config.js
@@ -3,3 +3,8 @@ export const BASE_URL =
 export const ORIGIN = __ENV.ORIGIN || "https://thames-dev.fuseclassroom.com";
 export const TOKEN = __ENV.TOKEN || "default_token_here";
 export const ID_TOKEN = __ENV.ID_TOKEN || "default_id_token_here";
+// Prod
+// export const BASE_URL =__ENV.BASE_URL || "https://app-api.fuseclassroom.com";
+// export const ORIGIN = __ENV.ORIGIN || "https://aishikshya.student.fuseclassroom.com";
+// export const TOKEN = __ENV.TOKEN || __ENV.TOKEN || "default_token_here";
+// export const ID_TOKEN = __ENV.ID_TOKEN || "default_id_token_here";
